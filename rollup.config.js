a266099c59b35f30d49abde79a9bb9fc0cebc9dd
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-02-24 18:18:50 -0300

    Work on build process
    - build only dialogplus.umd.js in dev mode
    - minified versions of each module format
    - sourcemaps
    - banner comment

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,50 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-export default [
-  {
+const getBanner = file => `\
+/** @preserve
+  * package: ${pkg.name} v${pkg.version}
+  * file: ${file}
+  * homepage: ${pkg.homepage}
+  * license: ${pkg.license}
+  **/\n`
+
+const getConfig = ({ minify, formats }) => {
+  return {
     input: 'src/dialogplus.js',
     external: [],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-      { file: pkg.browser, format: 'umd', name: 'dialogplus' },
-    ],
+    output: formats.map(format => {
+      const file = `dist/dialogplus.${format + (minify ? '.min' : '') + '.js'}`
+      return {
+        format,
+        file,
+        banner: getBanner(file),
+        sourcemap: true,
+        name: 'dialogplus',
+        globals: {},
+      }
+    }),
     plugins: [
       resolve(),
       babel({
         exclude: 'node_modules/**',
       }),
       commonjs(),
-    ],
-  },
-]
+      minify &&
+        terser({
+          output: {
+            comments: (_, { value }) => /@preserve/.test(value),
+          },
+        }),
+    ].filter(Boolean),
+  }
+}
+
+export default (process.env.IS_DEVELOPMENT_BUILD
+  ? getConfig({ minify: false, formats: ['umd'] })
+  : [false, true].map(minify =>
+      getConfig({ minify, formats: ['cjs', 'es', 'umd'] }),
+    ))
