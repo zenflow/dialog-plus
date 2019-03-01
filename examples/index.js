777211ca8c35ff9cc1d5bc749ea3d827c2163b85
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-03-01 03:21:03 -0300

    Use snabbdom for examples page

--- examples/index.js
+++ examples/index.js
@@ -1,35 +1,52 @@
+import { init } from 'snabbdom'
+import snabbdomModuleClass from 'snabbdom/modules/class'
+import snabbdomModuleProps from 'snabbdom/modules/props'
+import snabbdomModuleStyle from 'snabbdom/modules/style'
+import snabbdomModuleEventlisteners from 'snabbdom/modules/eventlisteners'
+import h from 'snabbdom/h'
 import { examples } from './examples'
 
-const mainEl = document.getElementById('main')
+const patch = init([
+  snabbdomModuleClass,
+  snabbdomModuleProps,
+  snabbdomModuleStyle,
+  snabbdomModuleEventlisteners,
+])
 
-const olEl = document.createElement('ol')
-mainEl.appendChild(olEl)
+let vnode = render()
+patch(document.getElementById('main'), vnode)
+function myPatch(next) {
+  vnode = patch(vnode, next)
+}
 
-for (const { id, desc, fn } of examples) {
-  const liEl = document.createElement('li')
-  olEl.appendChild(liEl)
-
-  const aEl = document.createElement('a')
-  aEl.innerText = id
-  aEl.id = id
-  aEl.href = `#${id}`
-  liEl.appendChild(aEl)
-
-  if (desc) {
-    const pEl = document.createElement('p')
-    pEl.innerText = desc
-    liEl.appendChild(pEl)
-  }
-
-  const preEl = document.createElement('pre')
-  preEl.innerText = fn.toString()
-  preEl.addEventListener('click', () => {
-    console.log(`"${id}": started`)
-    fn()
-      .catch(console.error)
-      .then(() => {
-        console.log(`"${id}": finished`)
-      })
+let selectedExample = null
+function render() {
+  const items = examples.map(example => {
+    return h('li', [
+      h('button', { on: { click: () => openExample(example) } }, [example.id]),
+      h(
+        'pre',
+        {
+          on: { click: () => runExample(example) },
+          style: { display: example === selectedExample ? 'block' : 'none' },
+        },
+        example.fn.toString(),
+      ),
+    ])
   })
-  liEl.appendChild(preEl)
+  return h('ol', items)
+}
+
+function openExample(example) {
+  selectedExample = example
+  myPatch(render())
+}
+
+function runExample({ id, fn }) {
+  console.log(`"${id}": started`)
+  fn()
+    .catch(console.error)
+    .then(() => {
+      console.log(`"${id}": finished`)
+    })
 }
